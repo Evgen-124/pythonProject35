8793e2c4779178594f3a39adb0f1ee9a00a583b3
Evgen-124 <Zhenya_panov_1994@mail.ru>
2024-11-05 20:29:34 +0300

    Module_3_2

---
+++ Module_3_2.py
@@ -0,0 +1,26 @@
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    def is_valid_email(email):
+        if "@" not in email:
+            return False
+        domain = email.split("@")[1]
+        if not domain.endswith((".com", ".ru", ".net")):
+            return False
+        return True
+
+    if not is_valid_email(sender) or not is_valid_email(recipient):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
